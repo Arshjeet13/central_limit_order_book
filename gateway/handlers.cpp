906efff1c70c95a32e6d4bb7b1e31ac96167c379
Arshjeet <arshjeet.bajwa@gmail.com>
2026-06-05 16:51:01 +0000

    Add handlers.cpp Incomplete code so far

---
+++ gateway/handlers.cpp
@@ -0,0 +1,8 @@
+#include "session.hpp"
+#include "boe/protocol.hpp"
+#include <cstdint>
+#include <utility>
+
+std::pair<boe::login_response, boe::replay_complete> handle_login_request(uint8_t* buffer){
+    
+}
