79987fc00fcc3fbb784e236da364566d2033f11c
Arshjeet <arshjeet.bajwa@gmail.com>
2026-06-05 16:46:48 +0000

    Add session struct definition to be used by all codefiles. Fixed datatype of buffer in recv_exact() declaration's

--- gateway/session.hpp
+++ gateway/session.hpp
@@ -2,14 +2,21 @@
 
 #include <cstdint>
 #include <cstring>
-#include "boe_protocol.hpp"
+
+struct Session
+{
+    int      fd{-1};
+    bool     logged_in{false};
+    uint32_t next_seq_num{1};
+    char     username[4]{};
+};
 
 // starts the recv loop for a connected client
 void run_session(int fd);
 
 // reads exactly n_bytes from fd into buf
 // returns false on disconnect or error
-bool recv_exact(int fd, void* buf, int n_bytes);
+bool recv_exact(int fd, uint8_t* buffer, int n_bytes);
 
 // sends a BOE message to the client
 void send_message(int fd, const void* data, int len);
