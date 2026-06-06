94aa98392e58146c84855b88d0c4b43d1913f634
Arshjeet <arshjeet.bajwa@gmail.com>
2026-06-06 08:16:23 +0000

    Remove helper methods in handler.cpp from handler.hpp

--- gateway/handlers.hpp
+++ gateway/handlers.hpp
@@ -6,18 +6,19 @@
 #include <utility>
 #include <variant>
 
+void send_message(uint8_t* buffer);
+
+// boe::login_response make_login_response(bool valid);
+
+// boe::replay_complete make_replay_complete();
+
 void handle_login_request(Session& session, const boe::login_request login_request);
 
-void handle_logout_request(Session& session, const boe::logout_request);
+void handle_logout_request(Session& session, const boe::logout_request logout_request);
 
-void handle_new_order(Session& session, const boe::new_order);
+void handle_new_order(Session& session, const boe::new_order new_order);
 
-void handle_cancel_order(Session& session, const boe::cancel_order);
+void handle_cancel_order(Session& session, const boe::cancel_order cancel_order);
 
-boe::login_response make_login_response(bool valid);
-
-boe::replay_complete make_replay_complete();
-
-void send_message(uint8_t* buffer);
 // boe::server_heartbeat send_server_heartbeat();
 // will be handled when basic functionality is achieved
